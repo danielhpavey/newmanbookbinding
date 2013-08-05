d8cc50ebf29d1ff8796702935dbf2c16abad55c0
Daniel H Pavey <danielhpavey@gmail.com>
2013-08-06 00:18:18 +0100

    More

--- wp-content/themes/newmanbookrestoration/home.php
+++ wp-content/themes/newmanbookrestoration/home.php
@@ -8,13 +8,15 @@ Template Name: Home Page
 ?>
 <?php //get_sidebar(); ?>
 <div class = "images">
+<div>
 <img src = "<?php bloginfo( 'template_url' ); ?>/images/book.jpg" />	
+</div>
 </div>
 <section>
 <?php if ( have_posts() ) : ?>
 <?php while ( have_posts() ) : the_post(); ?>
 <article>
-<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+<!--<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>-->
 </h2>
 <?php the_content(); ?>
 </article>
