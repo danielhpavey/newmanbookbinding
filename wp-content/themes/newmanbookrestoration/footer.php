02e43e9e5e619d91c07d38a8bb25da96537f8376
Daniel H Pavey <danielhpavey@gmail.com>
2013-08-21 21:36:12 +0100

    Fiddled with bxslider some more

--- wp-content/themes/newmanbookrestoration/footer.php
+++ wp-content/themes/newmanbookrestoration/footer.php
@@ -6,7 +6,11 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
-  $('.bxslider').bxSlider();
+	$( '.bxslider' ).bxSlider({
+//		  auto: true,
+		mode:'fade',
+		speed: 2000
+	})
 });
 </script>
 <script type = "text/javascript" src = "<?php bloginfo( 'template_url' ); ?>/bxslider/jquery.bxslider.min.js"/></script>
