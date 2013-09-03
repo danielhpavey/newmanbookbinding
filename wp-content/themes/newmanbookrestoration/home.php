77a05d3675875635b373209669d818e9dc00b410
Daniel H Pavey <danielhpavey@gmail.com>
2013-09-03 20:57:58 +0100

    added git ignore and continued design of site

--- wp-content/themes/newmanbookrestoration/home.php
+++ wp-content/themes/newmanbookrestoration/home.php
@@ -23,7 +23,7 @@ Template Name: Home Page
 <section>
 <?php if ( have_posts() ) : ?>
 <?php while ( have_posts() ) : the_post(); ?>
-<article>
+<article class = "home">
 <!--<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>-->
 </h2>
 <?php the_content(); ?>
