5f78b3dfde99cde21e50b3908269f9f61ad8f3ff
Daniel H Pavey <danielhpavey@gmail.com>
2014-02-05 22:13:12 +0000

    -amUsed php to include home page slider javascript only on home page

--- wp-content/themes/newmanbookrestoration/footer.php
+++ wp-content/themes/newmanbookrestoration/footer.php
@@ -1,6 +1,12 @@
 <footer>
 <?php wp_nav_menu( array( 'menu' => 'header', 'container_class' => 'footer-menu' ) ); ?>
+<?php wp_footer(); ?>
 </footer>  
+
+<?php
+$page_id     = get_queried_object_id();
+if ( $page_id == 4 ){
+?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
@@ -12,5 +18,8 @@ $(document).ready(function(){
 });
 </script>
 <script type = "text/javascript" src = "<?php bloginfo( 'template_url' ); ?>/bxslider/jquery.bxslider.min.js"/></script>
+<?php
+}
+?>
 </body>
 </html>
